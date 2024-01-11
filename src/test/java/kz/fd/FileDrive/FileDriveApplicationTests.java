e30a4bea1a742a6b364c0b87d3188fa8bacc7678
Асланбек С. Кулантаев <asku@kazinfosystems.kz>
2024-01-11 10:50:44 +0600

    init project

---
+++ src/test/java/kz/fd/FileDrive/FileDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.fd.FileDrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
